42673d3a9b4b1b95effe8223e3f5a0bdf750fad8
joshualim92 <joshualim92@icloud.com>
2017-09-27 12:09:12 -0700

    Add .swp file find command aliases

--- zsh-custom/aliases.zsh
+++ zsh-custom/aliases.zsh
@@ -3,6 +3,10 @@ alias ctags="`brew --prefix`/bin/ctags" # call correct ctags
 # Git
 alias gbv="git branch -a -vv"
 
+# Vim
+alias swpl="find . -name \"*.swp\""
+alias swpr="find . -name \"*.swp\" -exec rm {} +"
+
 # Emacs
 alias es="emacsclient -c"
 alias emac="emacsclient"
