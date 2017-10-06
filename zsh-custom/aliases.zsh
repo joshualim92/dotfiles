0cca2348b5e2aaddfd255d7da8cd560689ac2052
joshualim92 <joshualim92@icloud.com>
2017-10-06 12:18:04 -0700

    Add grbia alias

--- zsh-custom/aliases.zsh
+++ zsh-custom/aliases.zsh
@@ -2,6 +2,7 @@ alias ctags="`brew --prefix`/bin/ctags" # call correct ctags
 
 # Git
 alias gbv="git branch -a -vv"
+alias grbia="git rebase -i --autosquash"
 
 # Vim
 alias swpl="find . -name \"*.swp\""
