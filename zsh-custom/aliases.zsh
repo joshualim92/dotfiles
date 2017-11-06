fa934e6b844676b792ce3e137e8f4f28379f1af6
joshualim92 <joshualim92@icloud.com>
2017-11-06 10:51:00 -0800

    Add tmux aliases

--- zsh-custom/aliases.zsh
+++ zsh-custom/aliases.zsh
@@ -5,6 +5,14 @@ alias sb16t="setbase16theme"
 # Git
 alias gbv="git branch -a -vv"
 
+# Tmux
+alias tl="tmux ls"
+alias tn="tmux new -s"
+alias tns="tmux new-session -s"
+alias tks="tmux kill-session -t"
+alias ta="tmux a"
+alias tat="tmux a -t"
+
 # Vim
 alias swpl="find . -name \"*.swp\""
 alias swpr="find . -name \"*.swp\" -exec rm -i {} +"
