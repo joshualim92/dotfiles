89cab70794e00e8ac44703d5c9767d1130f33fb2
joshualim92 <joshualim92@icloud.com>
2017-11-10 09:42:48 -0800

    Reorganize shell aliases
     - Sort alphabetically
    
     - Remove tmux new-session since new is an alias for it

--- zsh-custom/aliases.zsh
+++ zsh-custom/aliases.zsh
@@ -2,19 +2,18 @@ alias ctags="`brew --prefix`/bin/ctags" # call correct ctags
 
 alias sb16t="setbase16theme"
 
+# Emacs
+alias emac="emacsclient"
+alias es="emacsclient -c"
+
 # Tmux
-alias tl="tmux ls"
-alias tn="tmux new -s"
-alias tns="tmux new-session -s"
-alias tks="tmux kill-session -t"
 alias ta="tmux a"
 alias tat="tmux a -t"
 alias td="tmux detach"
+alias tks="tmux kill-session -t"
+alias tl="tmux ls"
+alias tn="tmux new -s"
 
 # Vim
 alias swpl="find . -name \"*.swp\""
 alias swpr="find . -name \"*.swp\" -exec rm -i {} +"
-
-# Emacs
-alias es="emacsclient -c"
-alias emac="emacsclient"
