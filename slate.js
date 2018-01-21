a873b90dda43e22f7091d143728fc795f59a1070
Josh Lim <joshualim92@icloud.com>
2018-01-21 13:52:15 -0800

    Replace slate with slate.js

--- slate.js
+++ slate.js
@@ -1,53 +1,92 @@
+S.configAll({
+	"defaultToCurrentScreen": true,
+	"nudgePercentOf": "screenSize",
+	"resizePercentOf": "screenSize",
+	"windowHintsIgnoreHiddenWindows": true,
+	"windowHintsShowIcons": false,
+	"windowHintsSpread": true,
+});
+
 // Store current window states.
 var windowStates = {};
 
+var full = S.op("move", {
+	"x" : "screenOriginX",
+	"y" : "screenOriginY",
+	"width" : "screenSizeX",
+	"height" : "screenSizeY",
+});
+var topLeft = S.op("corner", {
+	"direction": "top-left",
+	"width": "screenSizeX/2",
+	"height": "screenSizeY/2",
+});
+var topRight = S.op("corner", {
+	"direction": "top-right",
+	"width": "screenSizeX/2",
+	"height": "screenSizeY/2",
+});
+var bottomLeft = S.op("corner", {
+	"direction": "bottom-left",
+	"width": "screenSizeX/2",
+	"height": "screenSizeY/2",
+});
+var bottomRight = S.op("corner", {
+	"direction": "bottom-right",
+	"width": "screenSizeX/2",
+	"height": "screenSizeY/2",
+});
+// Left
 var leftBarThird = S.op("push", {
 	"direction": "left",
-	"style": "bar-resize:screenSizeX/3"
+	"style": "bar-resize:screenSizeX/3",
 });
 var leftBarHalf = S.op("push", {
 	"direction": "left",
-	"style": "bar-resize:screenSizeX/2"
+	"style": "bar-resize:screenSizeX/2",
 });
 var leftBarTwoThirds = S.op("push", {
 	"direction": "left",
-	"style": "bar-resize:screenSizeX/3*2"
+	"style": "bar-resize:screenSizeX/3*2",
 });
+// Right
 var rightBarThird = S.op("push", {
 	"direction": "right",
-	"style": "bar-resize:screenSizeX/3"
+	"style": "bar-resize:screenSizeX/3",
 });
 var rightBarHalf = S.op("push", {
 	"direction": "right",
-	"style": "bar-resize:screenSizeX/2"
+	"style": "bar-resize:screenSizeX/2",
 });
 var rightBarTwoThirds = S.op("push", {
 	"direction": "right",
-	"style": "bar-resize:screenSizeX/3*2"
+	"style": "bar-resize:screenSizeX/3*2",
 });
+// Top
 var topBarThird = S.op("push", {
 	"direction": "top",
-	"style": "bar-resize:screenSizeY/3"
+	"style": "bar-resize:screenSizeY/3",
 });
 var topBarHalf = S.op("push", {
 	"direction": "top",
-	"style": "bar-resize:screenSizeY/2"
+	"style": "bar-resize:screenSizeY/2",
 });
 var topBarTwoThirds = S.op("push", {
 	"direction": "top",
-	"style": "bar-resize:screenSizeY/3*2"
+	"style": "bar-resize:screenSizeY/3*2",
 });
+// Bottom
 var bottomBarThird = S.op("push", {
 	"direction": "bottom",
-	"style": "bar-resize:screenSizeY/3"
+	"style": "bar-resize:screenSizeY/3",
 });
 var bottomBarHalf = S.op("push", {
 	"direction": "bottom",
-	"style": "bar-resize:screenSizeY/2"
+	"style": "bar-resize:screenSizeY/2",
 });
 var bottomBarTwoThirds = S.op("push", {
 	"direction": "bottom",
-	"style": "bar-resize:screenSizeY/3*2"
+	"style": "bar-resize:screenSizeY/3*2",
 });
 
 function getCycleStates(states) {
@@ -67,14 +106,35 @@ function getCycleStates(states) {
 	};
 }
 
-// Window cycling functions.
+// Window cycling functions
 var leftCycleOp = getCycleStates(['leftBarHalf', 'leftBarTwoThirds', 'leftBarThird']);
 var rightCycleOp = getCycleStates(['rightBarHalf', 'rightBarTwoThirds', 'rightBarThird']);
 var topCycleOp = getCycleStates(['topBarHalf', 'topBarTwoThirds', 'topBarThird']);
 var bottomCycleOp = getCycleStates(['bottomBarHalf', 'bottomBarTwoThirds', 'bottomBarThird']);
 
-// Bind the window cycling functions.
+// Push bindings
 S.bind('j:ctrl,cmd', leftCycleOp);
 S.bind('l:ctrl,cmd', rightCycleOp);
 S.bind('i:ctrl,cmd', topCycleOp);
 S.bind('k:ctrl,cmd', bottomCycleOp);
+S.bind('u:ctrl,cmd', topLeft);
+S.bind('o:ctrl,cmd', topRight);
+S.bind('m:ctrl,cmd', bottomLeft);
+S.bind('.:ctrl,cmd', bottomRight);
+S.bind(';:ctrl,cmd', full);
+
+// Throw bindings
+S.bind("j:ctrl;alt;cmd", S.op("throw", { screen: "left"  }));
+S.bind("i:ctrl;alt;cmd", S.op("throw", { screen: "up"    }));
+S.bind("l:ctrl;alt;cmd", S.op("throw", { screen: "right" }));
+S.bind("k:ctrl;alt;cmd", S.op("throw", { screen: "below" }));
+S.bind(";:ctrl;alt;cmd", full);
+
+// Focus bindings
+S.bind("j:ctrl;alt", S.op("focus", { direction: "left"  }));
+S.bind("i:ctrl;alt", S.op("focus", { direction: "up"    }));
+S.bind("l:ctrl;alt", S.op("focus", { direction: "right" }));
+S.bind("k:ctrl;alt", S.op("focus", { direction: "down"  }));
+
+// Hint binding
+S.bind(";:ctrl;alt", S.op("hint", { characters: "ASDFGHJKL" }));
