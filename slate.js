b7d59e514b9bfa0913d0df5b263858079d904cc1
Josh Lim <joshualim92@icloud.com>
2018-08-02 21:14:31 -0700

    Add other slate keybindings back in

--- slate.js
+++ slate.js
@@ -48,96 +48,96 @@ var rightBarTwoThirds = S.op("push", {
   direction: "right",
   style: "bar-resize:screenSizeX/3*2"
 });
-// // Top
-// var topBarThird = S.op('push', {
-// 	'direction': 'top',
-// 	'style': 'bar-resize:screenSizeY/3',
-// });
-// var topBarHalf = S.op('push', {
-// 	'direction': 'top',
-// 	'style': 'bar-resize:screenSizeY/2',
-// });
-// var topBarTwoThirds = S.op('push', {
-// 	'direction': 'top',
-// 	'style': 'bar-resize:screenSizeY/3*2',
-// });
-// // Bottom
-// var bottomBarThird = S.op('push', {
-// 	'direction': 'bottom',
-// 	'style': 'bar-resize:screenSizeY/3',
-// });
-// var bottomBarHalf = S.op('push', {
-// 	'direction': 'bottom',
-// 	'style': 'bar-resize:screenSizeY/2',
-// });
-// var bottomBarTwoThirds = S.op('push', {
-// 	'direction': 'bottom',
-// 	'style': 'bar-resize:screenSizeY/3*2',
-// });
-// // Top Left
-// var topLeftCorner = S.op('corner', {
-// 	'direction': 'top-left',
-// 	'width': 'screenSizeX/2',
-// 	'height': 'screenSizeY/2',
-// });
-// var topLeftTwoThird = S.op('corner', {
-// 	'direction': 'top-left',
-// 	'width': 'screenSizeX/3*2',
-// 	'height': 'screenSizeY/3*2',
-// });
-// var topLeftThird = S.op('corner', {
-// 	'direction': 'top-left',
-// 	'width': 'screenSizeX/3',
-// 	'height': 'screenSizeY/3',
-// });
-// // Top Right
-// var topRightCorner = S.op('corner', {
-// 	'direction': 'top-right',
-// 	'width': 'screenSizeX/2',
-// 	'height': 'screenSizeY/2',
-// });
-// var topRightTwoThird = S.op('corner', {
-// 	'direction': 'top-right',
-// 	'width': 'screenSizeX/3*2',
-// 	'height': 'screenSizeY/3*2',
-// });
-// var topRightThird = S.op('corner', {
-// 	'direction': 'top-right',
-// 	'width': 'screenSizeX/3',
-// 	'height': 'screenSizeY/3',
-// });
-// // Bottom Left
-// var bottomLeftCorner = S.op('corner', {
-// 	'direction': 'bottom-left',
-// 	'width': 'screenSizeX/2',
-// 	'height': 'screenSizeY/2',
-// });
-// var bottomLeftTwoThird = S.op('corner', {
-// 	'direction': 'bottom-left',
-// 	'width': 'screenSizeX/3*2',
-// 	'height': 'screenSizeY/3*2',
-// });
-// var bottomLeftThird = S.op('corner', {
-// 	'direction': 'bottom-left',
-// 	'width': 'screenSizeX/3',
-// 	'height': 'screenSizeY/3',
-// });
-// // Bottom Right
-// var bottomRightCorner = S.op('corner', {
-// 	'direction': 'bottom-right',
-// 	'width': 'screenSizeX/2',
-// 	'height': 'screenSizeY/2',
-// });
-// var bottomRightTwoThird = S.op('corner', {
-// 	'direction': 'bottom-right',
-// 	'width': 'screenSizeX/3*2',
-// 	'height': 'screenSizeY/3*2',
-// });
-// var bottomRightThird = S.op('corner', {
-// 	'direction': 'bottom-right',
-// 	'width': 'screenSizeX/3',
-// 	'height': 'screenSizeY/3',
-// });
+// Top
+var topBarThird = S.op('push', {
+	'direction': 'top',
+	'style': 'bar-resize:screenSizeY/3',
+});
+var topBarHalf = S.op('push', {
+	'direction': 'top',
+	'style': 'bar-resize:screenSizeY/2',
+});
+var topBarTwoThirds = S.op('push', {
+	'direction': 'top',
+	'style': 'bar-resize:screenSizeY/3*2',
+});
+// Bottom
+var bottomBarThird = S.op('push', {
+	'direction': 'bottom',
+	'style': 'bar-resize:screenSizeY/3',
+});
+var bottomBarHalf = S.op('push', {
+	'direction': 'bottom',
+	'style': 'bar-resize:screenSizeY/2',
+});
+var bottomBarTwoThirds = S.op('push', {
+	'direction': 'bottom',
+	'style': 'bar-resize:screenSizeY/3*2',
+});
+// Top Left
+var topLeftCorner = S.op('corner', {
+	'direction': 'top-left',
+	'width': 'screenSizeX/2',
+	'height': 'screenSizeY/2',
+});
+var topLeftTwoThird = S.op('corner', {
+	'direction': 'top-left',
+	'width': 'screenSizeX/3*2',
+	'height': 'screenSizeY/3*2',
+});
+var topLeftThird = S.op('corner', {
+	'direction': 'top-left',
+	'width': 'screenSizeX/3',
+	'height': 'screenSizeY/3',
+});
+// Top Right
+var topRightCorner = S.op('corner', {
+	'direction': 'top-right',
+	'width': 'screenSizeX/2',
+	'height': 'screenSizeY/2',
+});
+var topRightTwoThird = S.op('corner', {
+	'direction': 'top-right',
+	'width': 'screenSizeX/3*2',
+	'height': 'screenSizeY/3*2',
+});
+var topRightThird = S.op('corner', {
+	'direction': 'top-right',
+	'width': 'screenSizeX/3',
+	'height': 'screenSizeY/3',
+});
+// Bottom Left
+var bottomLeftCorner = S.op('corner', {
+	'direction': 'bottom-left',
+	'width': 'screenSizeX/2',
+	'height': 'screenSizeY/2',
+});
+var bottomLeftTwoThird = S.op('corner', {
+	'direction': 'bottom-left',
+	'width': 'screenSizeX/3*2',
+	'height': 'screenSizeY/3*2',
+});
+var bottomLeftThird = S.op('corner', {
+	'direction': 'bottom-left',
+	'width': 'screenSizeX/3',
+	'height': 'screenSizeY/3',
+});
+// Bottom Right
+var bottomRightCorner = S.op('corner', {
+	'direction': 'bottom-right',
+	'width': 'screenSizeX/2',
+	'height': 'screenSizeY/2',
+});
+var bottomRightTwoThird = S.op('corner', {
+	'direction': 'bottom-right',
+	'width': 'screenSizeX/3*2',
+	'height': 'screenSizeY/3*2',
+});
+var bottomRightThird = S.op('corner', {
+	'direction': 'bottom-right',
+	'width': 'screenSizeX/3',
+	'height': 'screenSizeY/3',
+});
 
 function getCycleStates(states) {
   return function(win) {
@@ -171,111 +171,111 @@ var rightCycleOp = getCycleStates([
   "rightBarTwoThirds",
   "rightBarThird"
 ]);
-// var topCycleOp = getCycleStates([
-//   "topBarHalf",
-//   "topBarTwoThirds",
-//   "topBarThird"
-// ]);
-// var bottomCycleOp = getCycleStates([
-//   "bottomBarHalf",
-//   "bottomBarTwoThirds",
-//   "bottomBarThird"
-// ]);
-// var topLeftCornerCycleOp = getCycleStates([
-//   "topLeftCorner",
-//   "topLeftTwoThird",
-//   "topLeftThird"
-// ]);
-// var topRightCornerCycleOp = getCycleStates([
-//   "topRightCorner",
-//   "topRightTwoThird",
-//   "topRightThird"
-// ]);
-// var bottomLeftCornerCycleOp = getCycleStates([
-//   "bottomLeftCorner",
-//   "bottomLeftTwoThird",
-//   "bottomLeftThird"
-// ]);
-// var bottomRightCornerCycleOp = getCycleStates([
-//   "bottomRightCorner",
-//   "bottomRightTwoThird",
-//   "bottomRightThird"
-// ]);
+var topCycleOp = getCycleStates([
+  "topBarHalf",
+  "topBarTwoThirds",
+  "topBarThird"
+]);
+var bottomCycleOp = getCycleStates([
+  "bottomBarHalf",
+  "bottomBarTwoThirds",
+  "bottomBarThird"
+]);
+var topLeftCornerCycleOp = getCycleStates([
+  "topLeftCorner",
+  "topLeftTwoThird",
+  "topLeftThird"
+]);
+var topRightCornerCycleOp = getCycleStates([
+  "topRightCorner",
+  "topRightTwoThird",
+  "topRightThird"
+]);
+var bottomLeftCornerCycleOp = getCycleStates([
+  "bottomLeftCorner",
+  "bottomLeftTwoThird",
+  "bottomLeftThird"
+]);
+var bottomRightCornerCycleOp = getCycleStates([
+  "bottomRightCorner",
+  "bottomRightTwoThird",
+  "bottomRightThird"
+]);
 
-// // 1/8th grids
-// var topLeftSubLeft = S.op("move", {
-//   x: "screenOriginX",
-//   y: "screenOriginY",
-//   width: "windowSizeX",
-//   height: "windowSizeY"
-// });
-// var topLeftSubRight = S.op("move", {
-//   x: "screenOriginX+screenSizeX/4",
-//   y: "screenOriginY",
-//   width: "screenSizeX/4",
-//   height: "windowSizeY"
-// });
-// var topRightSubLeft = S.op("move", {
-//   x: "screenOriginX+screenSizeX/2",
-//   y: "screenOriginY",
-//   width: "screenSizeX/4",
-//   height: "windowSizeY"
-// });
-// var topRightSubRight = S.op("move", {
-//   x: "screenOriginX+screenSizeX/4*3",
-//   y: "screenOriginY",
-//   width: "screenSizeX/4",
-//   height: "windowSizeY"
-// });
-// var bottomLeftSubLeft = S.op("move", {
-//   x: "screenOriginX",
-//   y: "screenOriginY+screenSizeY-windowSizeY",
-//   width: "screenSizeX/4",
-//   height: "windowSizeY"
-// });
-// var bottomLeftSubRight = S.op("move", {
-//   x: "screenOriginX+screenSizeX/4",
-//   y: "screenOriginY+screenSizeY-windowSizeY",
-//   width: "screenSizeX/4",
-//   height: "windowSizeY"
-// });
-// var bottomRightSubLeft = S.op("move", {
-//   x: "screenOriginX+screenSizeX/2",
-//   y: "screenOriginY+screenSizeY-windowSizeY",
-//   width: "screenSizeX/4",
-//   height: "windowSizeY"
-// });
-// var bottomRightSubRight = S.op("move", {
-//   x: "screenOriginX+screenSizeX/4 * 3",
-//   y: "screenOriginY+screenSizeY-windowSizeY",
-//   width: "screenSizeX/4",
-//   height: "windowSizeY"
-// });
+// 1/8th grids
+var topLeftSubLeft = S.op("move", {
+  x: "screenOriginX",
+  y: "screenOriginY",
+  width: "windowSizeX",
+  height: "windowSizeY"
+});
+var topLeftSubRight = S.op("move", {
+  x: "screenOriginX+screenSizeX/4",
+  y: "screenOriginY",
+  width: "screenSizeX/4",
+  height: "windowSizeY"
+});
+var topRightSubLeft = S.op("move", {
+  x: "screenOriginX+screenSizeX/2",
+  y: "screenOriginY",
+  width: "screenSizeX/4",
+  height: "windowSizeY"
+});
+var topRightSubRight = S.op("move", {
+  x: "screenOriginX+screenSizeX/4*3",
+  y: "screenOriginY",
+  width: "screenSizeX/4",
+  height: "windowSizeY"
+});
+var bottomLeftSubLeft = S.op("move", {
+  x: "screenOriginX",
+  y: "screenOriginY+screenSizeY-windowSizeY",
+  width: "screenSizeX/4",
+  height: "windowSizeY"
+});
+var bottomLeftSubRight = S.op("move", {
+  x: "screenOriginX+screenSizeX/4",
+  y: "screenOriginY+screenSizeY-windowSizeY",
+  width: "screenSizeX/4",
+  height: "windowSizeY"
+});
+var bottomRightSubLeft = S.op("move", {
+  x: "screenOriginX+screenSizeX/2",
+  y: "screenOriginY+screenSizeY-windowSizeY",
+  width: "screenSizeX/4",
+  height: "windowSizeY"
+});
+var bottomRightSubRight = S.op("move", {
+  x: "screenOriginX+screenSizeX/4 * 3",
+  y: "screenOriginY+screenSizeY-windowSizeY",
+  width: "screenSizeX/4",
+  height: "windowSizeY"
+});
 
-// var topLeftCycleOp = getCycleStates(["topLeftSubLeft", "topLeftSubRight"]);
-// var topRightCycleOp = getCycleStates(["topRightSubLeft", "topRightSubRight"]);
-// var bottomLeftCycleOp = getCycleStates([
-//   "bottomLeftSubLeft",
-//   "bottomLeftSubRight"
-// ]);
-// var bottomRightCycleOp = getCycleStates([
-//   "bottomRightSubLeft",
-//   "bottomRightSubRight"
-// ]);
+var topLeftCycleOp = getCycleStates(["topLeftSubLeft", "topLeftSubRight"]);
+var topRightCycleOp = getCycleStates(["topRightSubLeft", "topRightSubRight"]);
+var bottomLeftCycleOp = getCycleStates([
+  "bottomLeftSubLeft",
+  "bottomLeftSubRight"
+]);
+var bottomRightCycleOp = getCycleStates([
+  "bottomRightSubLeft",
+  "bottomRightSubRight"
+]);
 
 // Push bindings
 S.bind("j:ctrl,cmd", leftCycleOp);
 S.bind("l:ctrl,cmd", rightCycleOp);
-// S.bind("i:ctrl,cmd", topCycleOp);
-// S.bind(",:ctrl,cmd", bottomCycleOp);
-// S.bind("u:ctrl,cmd", topLeftCornerCycleOp);
-// S.bind("o:ctrl,cmd", topRightCornerCycleOp);
-// S.bind("m:ctrl,cmd", bottomLeftCornerCycleOp);
-// S.bind(".:ctrl,cmd", bottomRightCornerCycleOp);
-// S.bind("y:ctrl,cmd", topLeftCycleOp);
-// S.bind("p:ctrl,cmd", topRightCycleOp);
-// S.bind("n:ctrl,cmd", bottomLeftCycleOp);
-// S.bind("/:ctrl,cmd", bottomRightCycleOp);
+S.bind("i:ctrl,cmd", topCycleOp);
+S.bind(",:ctrl,cmd", bottomCycleOp);
+S.bind("u:ctrl,cmd", topLeftCornerCycleOp);
+S.bind("o:ctrl,cmd", topRightCornerCycleOp);
+S.bind("m:ctrl,cmd", bottomLeftCornerCycleOp);
+S.bind(".:ctrl,cmd", bottomRightCornerCycleOp);
+S.bind("y:ctrl,cmd", topLeftCycleOp);
+S.bind("p:ctrl,cmd", topRightCycleOp);
+S.bind("n:ctrl,cmd", bottomLeftCycleOp);
+S.bind("/:ctrl,cmd", bottomRightCycleOp);
 S.bind("k:ctrl,cmd", centerCycleOp);
 S.bind(";:ctrl,cmd", fullCycleOp);
 
@@ -324,4 +324,4 @@ S.bind(";:ctrl,cmd", fullCycleOp);
 // Hint binding
 var hintChars = "JKLASDFGH";
 S.bind("space:ctrl;cmd", S.op("hint", { characters: hintChars }));
-// S.bind("space:ctrl;alt", S.op("hint", { characters: hintChars }));
+S.bind("space:ctrl;alt", S.op("hint", { characters: hintChars }));
